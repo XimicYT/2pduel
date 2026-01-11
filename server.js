b5ec15b0f71e57cb06a6480f0ce587b57acded03
XimicYT <abutts29@students.unorth.k12.in.us>
2026-01-10 20:32:54 -0500

    Update server.js via Web Editor

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 
 // ==================================================================
-// 1. SETUP SERVER PEE
+// 1. SETUP SERVER
 // ==================================================================
 const app = express();
 app.use(cors());
